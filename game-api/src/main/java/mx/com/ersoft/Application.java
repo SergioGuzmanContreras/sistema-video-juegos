4133b6fd410b6a8b342b353910705dce12cb114f
Erik Guzman <erik.guzman@gluo.mx>
2020-02-10 20:17:26 -0600

    Cambios en la estructura del proyecto

--- game-api/src/main/java/mx/com/ersoft/Application.java
+++ game-api/src/main/java/mx/com/ersoft/Application.java
@@ -1,12 +1,17 @@
 package mx.com.ersoft;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Erik Sergio Guzman Contreras 
+ * @created 10 feb. 2020
+**/
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
